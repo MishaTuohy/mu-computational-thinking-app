0762ae045aeb2391f1a55e5f8ea256ecad14f779
Misha <67553114+MishaTuohy@users.noreply.github.com>
2022-11-02 14:52:10 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD1yR5DEmcLq2S47H3VKaSu2JQf4-NEKjk",
-  authDomain: "mu-ct-app.firebaseapp.com",
-  projectId: "mu-ct-app",
-  storageBucket: "mu-ct-app.appspot.com",
-  messagingSenderId: "129253705611",
-  appId: "1:129253705611:web:3f028a829afe01b4c8d18e",
-  measurementId: "G-NSZS5S17N1"
+  apiKey: "AIzaSyBBsT46f97yy0IpFUgQYB0INJaIpBaC7NQ",
+  authDomain: "mu-computational-thinking-app.firebaseapp.com",
+  projectId: "mu-computational-thinking-app",
+  storageBucket: "mu-computational-thinking-app.appspot.com",
+  messagingSenderId: "408185952246",
+  appId: "1:408185952246:web:6603731e3cbabc6ef319d7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
