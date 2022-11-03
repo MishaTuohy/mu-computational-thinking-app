da3d27d36a9440ad706329368e67c1276cd89133
Misha <67553114+MishaTuohy@users.noreply.github.com>
2022-11-03 13:05:11 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBBsT46f97yy0IpFUgQYB0INJaIpBaC7NQ",
-  authDomain: "mu-computational-thinking-app.firebaseapp.com",
-  projectId: "mu-computational-thinking-app",
-  storageBucket: "mu-computational-thinking-app.appspot.com",
-  messagingSenderId: "408185952246",
-  appId: "1:408185952246:web:6603731e3cbabc6ef319d7"
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBBsT46f97yy0IpFUgQYB0INJaIpBaC7NQ',
+//   authDomain: 'mu-computational-thinking-app.firebaseapp.com',
+//   projectId: 'mu-computational-thinking-app',
+//   storageBucket: 'mu-computational-thinking-app.appspot.com',
+//   messagingSenderId: '408185952246',
+//   appId: '1:408185952246:web:6603731e3cbabc6ef319d7',
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
