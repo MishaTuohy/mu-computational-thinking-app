106cf6889f14fa799261a2f75d4fad43ec28e571
Misha <67553114+MishaTuohy@users.noreply.github.com>
2022-11-03 13:30:52 +0000

    update

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,5 @@
 import { AuthErrorCodes } from 'firebase/auth';
+
 export const txtEmail = document.querySelector('#txtEmail');
 export const txtPassword = document.querySelector('#txtPassword');
 export const btnLogin = document.querySelector('#btnLogin');
