589ece230809cea51b57adb0b22006b7d3c0de72
Misha <67553114+MishaTuohy@users.noreply.github.com>
2022-11-06 14:12:44 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/app/App';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBBsT46f97yy0IpFUgQYB0INJaIpBaC7NQ',
-//   authDomain: 'mu-computational-thinking-app.firebaseapp.com',
-//   projectId: 'mu-computational-thinking-app',
-//   storageBucket: 'mu-computational-thinking-app.appspot.com',
-//   messagingSenderId: '408185952246',
-//   appId: '1:408185952246:web:6603731e3cbabc6ef319d7',
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-    <h1>Welcome</h1>, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
