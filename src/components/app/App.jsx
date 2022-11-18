8d15181af1f03048804441d27b4c2588cfa6afd0
Misha <67553114+MishaTuohy@users.noreply.github.com>
2022-11-18 14:12:06 +0000

    clean repo of test, add new images + css

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,12 @@
-import "../../styles/index.scss";
-import Recipes from "../recipes/Recipes";
-import sword from "../../images/swc-sword.png";
-import swordSvg from "../../images/sword.svg";
+import "../../styles/index.css";
 
 const App = () => {
     return (
         <>
-            <section className="hero"></section>
             <main>
                 <section>
                     <h1>Ello</h1>
                 </section>
-                <img src={sword} alt="sword" width= "250" />
-                <img src={swordSvg} alt="sword" width= "250" />
-                <Recipes />
             </main>
         </>
     )
